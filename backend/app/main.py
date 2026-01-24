d939baeb3447ae5e7dd1245a372ea18a2cb758b6
Goyam02 <goyamjain02@gmail.com>
2026-01-25 01:04:45 +0530

    resolve mood analysis error

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,6 +14,7 @@ app.add_middleware(
         "http://localhost:3000",
         "http://localhost:5173",  # Vite default
         "http://127.0.0.1:3000",
+        "*",
     ],
     allow_credentials=True,
     allow_methods=["*"],
